5038874ab38d0ef63ebda1b0a20cdc0accd74f67
Elie309 <elie.saade432@gmail.com>
2024-10-19 00:08:26 +0300

    Add EmployeeEntity class for managing employee data

---
+++ app/Entities/EmployeeEntity.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entities;
+
+use CodeIgniter\Entity\Entity;
+
+class EmployeeEntity extends Entity
+{
+    protected $datamap = [
+        'id'           => 'employee_id',
+        'phone_number' => 'employee_number',
+        'email'        => 'employee_email',
+        'password'     => 'employee_password',
+        'first_name'   => 'employee_first_name',
+        'last_name'    => 'employee_last_name',
+        'role'         => 'employee_role',
+
+    ];
+    protected $dates   = ['employee_created_at', 'employee_updated_at', 'employee_deleted_at'];
+    protected $casts   = [
+        'employee_id'           => 'integer',
+        'employee_number'       => 'integer',
+        'employee_email'        => 'string',
+        'employee_password'     => 'string',
+        'employee_first_name'   => 'string',
+        'employee_last_name'    => 'string',
+        'employee_role'         => 'string',
+    ];
+
+
+    public function setPassword(string $pass)
+    {
+        $this->attributes['employee_password'] = password_hash($pass, PASSWORD_BCRYPT);
+
+        return $this;
+    }
+
+    public function verifyPassword($password)
+    {
+        return password_verify($password, $this->attributes['employee_password']);
+    }
+}
