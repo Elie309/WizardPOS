0d2a9de3ddb36b99038c8d125431fa462bed5f9b
Elie309 <elie.saade432@gmail.com>
2024-10-20 01:16:17 +0300

    Add seeders for managing clients, products, and categories

--- app/Database/Seeds/MainSeeder.php
+++ app/Database/Seeds/MainSeeder.php
@@ -9,5 +9,8 @@ class MainSeeder extends Seeder
     public function run()
     {
         $this->call('App\Database\Seeds\Settings\EmployeeSeeder');
+        $this->call('App\Database\Seeds\Products\CategorySeeder');
+        $this->call('App\Database\Seeds\Products\ProductSeeder');
+        $this->call('App\Database\Seeds\Clients\ClientSeeder');
     }
 }
