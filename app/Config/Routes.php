0157a9d17cdfca1987b0256b2506310685826f26
Elie309 <elie.saade432@gmail.com>
2024-11-01 18:03:05 +0200

    Adding routes products & categories

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -19,5 +19,27 @@ $routes->group('api', function ($routes) {
         $routes->get('unauthorized', 'Auth\AuthController::unauthorized');
     });
 
-    //Employee
+    //Products
+    $routes->group('products', function ($routes) {
+        $routes->get('getMenuProducts', 'Products\ProductsController::getMenuProducts');
+        $routes->get('search', 'Products\ProductsController::index');
+        $routes->get('sku/(:alphanum)', 'Products\ProductsController::getWithSKU/$1');
+
+        $routes->post('/', 'Products\ProductsController::create');
+        $routes->post('(:alphanum)', 'Products\ProductsController::update/$1');
+        $routes->delete('(:alphanum)', 'Products\ProductsController::delete/$1');
+
+
+    });
+
+    //Categories
+    $routes->group('categories', function ($routes) {
+        $routes->get('/', 'Categories\CategoriesController::index');
+        $routes->get('(:alphanum)', 'Categories\CategoriesController::show/$1');
+
+        //Need authorization
+        $routes->post('/', 'Categories\CategoriesController::create');
+        $routes->post('(:alphanum)', 'Categories\CategoriesController::update/$1');
+        $routes->delete('(:alphanum)', 'Categories\CategoriesController::delete/$1');
+    });
 });
