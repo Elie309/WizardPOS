808394640ab072971ceb8156ff26480a19bbb8f9
Elie309 <elie.saade432@gmail.com>
2024-11-21 21:17:05 +0200

    Refactor seeders to use Faker for generating random client and product data, increasing the number of clients and adjusting product creation logic.

--- app/Database/Seeds/Clients/ClientSeeder.php
+++ app/Database/Seeds/Clients/ClientSeeder.php
@@ -3,25 +3,25 @@
 namespace App\Database\Seeds\Clients;
 
 use CodeIgniter\Database\Seeder;
+use Faker\Factory;
 
 class ClientSeeder extends Seeder
 {
     public function run()
     {
+        $faker = Factory::create();
         $data = [];
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 50; $i++) {
             $data[] = [
-                'client_first_name' => 'Client ' . $i,
-                'client_last_name' => 'Client ' . $i,
-                'client_email' => 'client' . $i . '@gmail.com',
-                'client_phone_number' => '1234567890' . $i,
-                'client_address' => 'Client Address ' . $i,
-                'client_is_active' => 1,
+                'client_first_name' => $faker->firstName,
+                'client_last_name' => $faker->lastName,
+                'client_email' => $faker->email,
+                'client_phone_number' => $faker->phoneNumber,
+                'client_address' => $faker->address,
+                'client_is_active' => $faker->boolean,
             ];
-
         }
 
         $this->db->table('clients')->insertBatch($data);
-
     }
 }
--- app/Database/Seeds/Products/ProductSeeder.php
+++ app/Database/Seeds/Products/ProductSeeder.php
@@ -3,30 +3,29 @@
 namespace App\Database\Seeds\Products;
 
 use CodeIgniter\Database\Seeder;
+use Faker\Factory;
 
 class ProductSeeder extends Seeder
 {
-
-    
     public function run()
     {
+        $faker = Factory::create();
         $categories = $this->db->table('categories')->get()->getResult();
 
-        for ($i = 10; $i < 100; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             $randomCategory = $categories[array_rand($categories)]->category_id;
-        
-            $products[] = [
-                'product_sku' => "SKU$i",
-                'product_slug' => "product-$i",
 
-                'product_name' => "Product $i",
-                'product_description' => "This is a sample description for Product $i.",
-                'product_price' => mt_rand(500, 2500) / 100,  // Random price between $5.00 and $25.00
+            $products[] = [
+                'product_sku' => $faker->numberBetween(1000000000000, 9999999999999),
+                'product_slug' => $faker->slug,
+                'product_name' => $faker->word,
+                'product_description' => $faker->sentence,
+                'product_price' => $faker->randomFloat(2, 5, 25),  // Random price between $5.00 and $25.00
                 'product_category_id' => $randomCategory,
-                'product_production_date' => date('Y-m-d', strtotime("-$i days")),
-                'product_expiry_date' => date('Y-m-d', strtotime("+$i days")),
-                'product_show_in_menu' => random_int(0, 1),
-                'product_image' => "https://picsum.photos/seed/picsum/640/480",
+                'product_production_date' => $faker->dateTimeBetween('-1 year', 'now')->format('Y-m-d'),
+                'product_expiry_date' => $faker->dateTimeBetween('now', '+1 year')->format('Y-m-d'),
+                'product_show_in_menu' => $faker->boolean,
+                'product_image' => $faker->imageUrl(640, 480, 'product'),
                 'product_is_active' => 1,
             ];
         }
