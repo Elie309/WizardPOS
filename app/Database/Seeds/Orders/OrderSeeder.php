e29bda21ede755fea63612b00f31bce22a36ed89
Elie309 <elie.saade432@gmail.com>
2024-12-15 03:19:27 +0200

    Update seeders to generate orders and reservations for the entire month

--- app/Database/Seeds/Orders/OrderSeeder.php
+++ app/Database/Seeds/Orders/OrderSeeder.php
@@ -16,62 +16,73 @@ class OrderSeeder extends Seeder
         $productIds = $this->db->table('products')->select('product_id, product_price')->get()->getResultArray();
         $restaurantTables = $this->db->table('restaurant_tables')->select('table_id, table_name')->get()->getResultArray();
 
-        for ($i = 0; $i < 20; $i++) {
-            $orderSubtotal = 0;
-            $orderDiscount = 0;
-            $orderTax = 0;
-            $orderTotal = 0;
+        $currentMonth = date('m');
+        $currentYear = date('Y');
+        $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
 
-            $orderData = [
-                'order_client_id' => $faker->randomElement($clientIds)['client_id'],
-                'order_employee_id' => $faker->randomElement($employeeIds)['employee_id'],
-                'order_type' => $faker->randomElement(['take-away', 'dine-in', 'delivery']),
-                'order_reference' => str_replace(' ', '_', $faker->randomElement($restaurantTables)['table_name']) . '-' . $faker->unique()->numberBetween(1000, 9999),
-                // 'order_date' => date('Y-m-d'), 
-                'order_date' => date('Y-m-d'), 
-                'order_time' => $faker->time(),
-                'order_note' => $faker->optional()->text(),
-                'order_subtotal' => $orderSubtotal,
-                'order_discount' => $orderDiscount,
-                'order_tax' => $orderTax,
-                'order_total' => $orderTotal,
-                'order_status' => $faker->randomElement(['on-going', 'completed', 'cancelled']),
-            ];
+        for ($day = 1; $day <= $daysInMonth; $day++) {
+            for ($i = 0; $i < 20; $i++) {
+                $orderSubtotal = 0;
+                $orderDiscount = 0;
+                $orderTax = 0;
+                $orderTotal = 0;
 
-            $this->db->table('orders')->insert($orderData);
-            $orderId = $this->db->insertID();
-
-            for ($j = 0; $j < $faker->numberBetween(1, 5); $j++) {
-                $product = $faker->randomElement($productIds);
-                $quantity = $faker->numberBetween(1, 10);
-                $orderItemTotal = $product['product_price'] * $quantity;
-                $orderSubtotal += $orderItemTotal;
-
-                $orderItemData = [
-                    'order_id' => $orderId,
-                    'order_item_product_id' => $product['product_id'],
-                    'order_item_quantity' => $quantity,
-                    'order_item_total' => $orderItemTotal,
+                $orderData = [
+                    'order_client_id' => $faker->randomElement($clientIds)['client_id'],
+                    'order_employee_id' => $faker->randomElement($employeeIds)['employee_id'],
+                    'order_type' => $faker->randomElement(['take-away', 'dine-in', 'delivery']),
+                    'order_reference' => str_replace(' ', '_', $faker->randomElement($restaurantTables)['table_name']) . '-' . $faker->unique()->numberBetween(1000, 9999),
+                    'order_date' => date('Y-m-d', strtotime("$currentYear-$currentMonth-$day")),
+                    'order_time' => $faker->time(),
+                    'order_note' => $faker->optional()->text(),
+                    'order_subtotal' => $orderSubtotal,
+                    'order_discount' => $orderDiscount,
+                    'order_tax' => $orderTax,
+                    'order_total' => $orderTotal,
+                    'order_status' => $faker->randomElement(
+                        array_merge(
+                            array_fill(0, 10, 'on-going'),
+                            array_fill(0, 8, 'completed'),
+                            array_fill(0, 2, 'cancelled')
+                        )
+                    ),
                 ];
 
-                $this->db->table('order_items')->insert($orderItemData);
+                $this->db->table('orders')->insert($orderData);
+                $orderId = $this->db->insertID();
+
+                for ($j = 0; $j < $faker->numberBetween(1, 5); $j++) {
+                    $product = $faker->randomElement($productIds);
+                    $quantity = $faker->numberBetween(1, 10);
+                    $orderItemTotal = $product['product_price'] * $quantity;
+                    $orderSubtotal += $orderItemTotal;
+
+                    $orderItemData = [
+                        'order_id' => $orderId,
+                        'order_item_product_id' => $product['product_id'],
+                        'order_item_quantity' => $quantity,
+                        'order_item_total' => $orderItemTotal,
+                    ];
+
+                    $this->db->table('order_items')->insert($orderItemData);
+                }
+
+                // Update order totals after inserting order items
+                $orderTax = $orderSubtotal * 0.11;
+                $orderTotal = $orderSubtotal + $orderTax;
+                $orderDiscount = $faker->randomFloat(2, 0, (($orderTotal /2) * 0.5));
+
+                $orderSubtotal = $orderSubtotal - $orderDiscount;
+                $orderTax = $orderSubtotal * 0.11;
+                $orderTotal = $orderSubtotal + $orderTax;
+
+                $this->db->table('orders')->update([
+                    'order_subtotal' => $orderSubtotal,
+                    'order_discount' => $orderDiscount,
+                    'order_tax' => $orderTax,
+                    'order_total' => $orderTotal
+                ], ['order_id' => $orderId]);
             }
-
-            // Update order totals after inserting order items
-            $orderTax = $orderSubtotal * 0.11;
-            $orderTotal = $orderSubtotal + $orderTax;
-            $orderDiscount = $faker->randomFloat(2, 0, (($orderTotal /2) * 0.5));
-
-            $orderSubtotal = $orderSubtotal - $orderDiscount;
-            $orderTax = $orderSubtotal * 0.11;
-            $orderTotal = $orderSubtotal + $orderTax;
-
-            $this->db->table('orders')->update([
-                'order_subtotal' => $orderSubtotal,
-                'order_discount' => $orderDiscount,
-                'order_tax' => $orderTax,
-                'order_total' => $orderTotal
-            ], ['order_id' => $orderId]);
         }
     }
 }
--- app/Database/Seeds/Reservations/ReservationSeeder.php
+++ app/Database/Seeds/Reservations/ReservationSeeder.php
@@ -4,7 +4,6 @@ namespace App\Database\Seeds\Reservations;
 
 use CodeIgniter\Database\Seeder;
 
-
 class ReservationSeeder extends Seeder
 {
     public function run()
@@ -21,31 +20,37 @@ class ReservationSeeder extends Seeder
         // Get data from employees table
         $employees = $this->db->table('employees')->select('employee_id')->get()->getResultArray();
 
-        // Insert fake reservations for the current day
+        $currentMonth = date('m');
+        $currentYear = date('Y');
+        $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
+
+        // Insert fake reservations for the entire month
         foreach ($tables as $table) {
-            $reservationsCount = $faker->numberBetween(1, 4);
-            for ($i = 0; $i < $reservationsCount; $i++) {
+            for ($day = 1; $day <= $daysInMonth; $day++) {
+                $reservationsCount = $faker->numberBetween(1, 4);
+                for ($i = 0; $i < $reservationsCount; $i++) {
 
-                //faker time
-                $startingTime = $faker->time();
-                //Add 1 to 3 hours randomly
-                $startingDateTime = new \DateTime($startingTime);
-                $interval = new \DateInterval('PT' . $faker->numberBetween(1, 3) . 'H');
-                $startingDateTime->add($interval);
-                $endingTime = $startingDateTime->format('H:i:s');
+                    //faker time
+                    $startingTime = $faker->time();
+                    //Add 1 to 3 hours randomly
+                    $startingDateTime = new \DateTime($startingTime);
+                    $interval = new \DateInterval('PT' . $faker->numberBetween(1, 3) . 'H');
+                    $startingDateTime->add($interval);
+                    $endingTime = $startingDateTime->format('H:i:s');
 
-                $this->db->table('reservations')->insert([
-                    'reservation_client_id' => $faker->randomElement($clients)['client_id'],
-                    'reservation_table_id' => $table['table_id'],
-                    'reservation_employee_id' => $faker->randomElement($employees)['employee_id'],
-                    'reservation_date' => date('Y-m-d'),
-                    'reservation_starting_time' => $startingTime,
-                    'reservation_ending_time' => $endingTime,
-                    'reservation_guests' => $faker->optional()->numberBetween(1, 10),
-                    'reservation_status' => $faker->randomElement(['pending', 'confirmed', 'cancelled', 'completed']),
-                    'reservation_created_at' => $faker->dateTime()->format('Y-m-d H:i:s'),
-                    'reservation_updated_at' => $faker->dateTime()->format('Y-m-d H:i:s'),
-                ]);
+                    $this->db->table('reservations')->insert([
+                        'reservation_client_id' => $faker->randomElement($clients)['client_id'],
+                        'reservation_table_id' => $table['table_id'],
+                        'reservation_employee_id' => $faker->randomElement($employees)['employee_id'],
+                        'reservation_date' => date('Y-m-d', strtotime("$currentYear-$currentMonth-$day")),
+                        'reservation_starting_time' => $startingTime,
+                        'reservation_ending_time' => $endingTime,
+                        'reservation_guests' => $faker->optional()->numberBetween(1, 10),
+                        'reservation_status' => $faker->randomElement(['pending', 'confirmed', 'cancelled', 'completed']),
+                        'reservation_created_at' => $faker->dateTime()->format('Y-m-d H:i:s'),
+                        'reservation_updated_at' => $faker->dateTime()->format('Y-m-d H:i:s'),
+                    ]);
+                }
             }
         }
     }
